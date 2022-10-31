5ad38ef4e32021183ed22d2b6ac381bcaefa5699
Mikha38 <oyks1379@yandex.ru>
2022-10-31 17:18:30 +0300

    home

--- src/Components/TopMenu/TopMenu.tsx
+++ src/Components/TopMenu/TopMenu.tsx
@@ -1,7 +1,12 @@
-import { Navbar } from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { useState } from "react";
 import {useLinkClickHandler, useLocation} from "react-router-dom";
 
 export default function TopMenu(){
+  const userId = false
+  const [authFromVisible, setAuthFormVisible] = useState<boolean>(false)
+  const [login, setLogin] = useState<string>('')
+  const [pwd, setPwd] = useState<string>('')
   const location = useLocation();
   const goMain = useLinkClickHandler("/");
     return (
@@ -18,7 +23,20 @@ export default function TopMenu(){
                   <Navbar.Link href="/" active={location.pathname === '/'} color='dark'>
                   Группы слов
                   </Navbar.Link>
+              </span>
+              <span className="cursor-pointer">
+                {!userId && !authFromVisible ?
+                  <span onClick={()=>setAuthFormVisible(!authFromVisible)}>
+                    {userId ? 'Выход' : 'Вход / Регистрация'}
+                  </span>
+                :
+                <span>
+                  <input type={'text'} value={login} onChange={(e)=>setLogin(e.target.value)}/>
+                  <input type={'text'} value={pwd} onChange={(e)=>setPwd(e.target.value)}/>
+                  <button>go!</button>
                 </span>
+                }
+              </span>
             </Navbar.Collapse>
         </Navbar>
       </div>
